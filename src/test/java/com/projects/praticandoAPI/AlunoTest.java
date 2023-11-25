e4982b968976a658f1d354bd5637456647faaa06
Santiago <sammysan007@gmail.com>
2023-11-25 20:24:41 -0300

    first commit

---
+++ src/test/java/com/projects/praticandoAPI/AlunoTest.java
@@ -0,0 +1,86 @@
+package com.projects.praticandoAPI;
+
+import static org.junit.Assert.assertEquals;
+
+import java.io.Serial;
+import java.util.ArrayList;
+import java.util.List;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+class AlunoTest {
+	
+	@Test
+	public void testAlunoNaoTemTresCursos() {
+		Aluno aluno = new Aluno("Jorgi", 20, false);
+		
+		Curso curso1 = new Curso("Calculo I", 0, false);
+		Curso curso2 = new Curso("Calculo II", 0, false);
+		Curso curso3 = new Curso("Calculo III", 0, false);
+		
+		aluno.addCurso(curso1);
+		aluno.addCurso(curso2);
+		aluno.addCurso(curso3);
+		
+		Curso curso4 = new Curso("Fenomenos Eletricos", 0, false);
+		
+		assertEquals(false, aluno.addCurso(curso4));
+	}
+	
+	@Test
+	public void testAlunoNaoTemMoedasSuficientes() {
+		Aluno aluno = new Aluno("Jorgi", 3, false);
+		
+		Curso curso = new Curso("Fenomenos Eletricos", 5, true);
+		
+		assertEquals(false, aluno.addCurso(curso));
+	}
+	
+	@Test
+	public void testAlunoRecebePremiumCom12Cursos() {
+		Aluno aluno = new Aluno("Jonas", 15, false);
+		List<Curso> cursos = new ArrayList<>();
+		
+		for (int i = 1; i <= 12; i++) {
+			Curso curso = new Curso("Curso " + i, 50, false);
+			cursos.add(curso);
+		}
+		aluno.setCursos(cursos);
+
+		assertEquals(true, aluno.verCursosETornarPremium());
+	}
+	
+	@Test
+	public void testAlunoRecebePremiumSem12Cursos() {
+		Aluno aluno = new Aluno("Jonas", 15, false);
+		List<Curso> cursos = new ArrayList<>();
+		
+		for (int i = 1; i <= 11; i++) {
+			Curso curso = new Curso("Curso " + i, 50, false);
+			cursos.add(curso);
+		}
+		aluno.setCursos(cursos);
+
+		assertEquals(false, aluno.verCursosETornarPremium());
+	}
+
+	@Test
+	public void testAlunoRecebe3MoedasCom12Cursos() {
+		Aluno aluno = new Aluno("Joaquim", 0, false);
+		List<Curso> cursos = new ArrayList<>();;
+		
+		int moedasBckp = aluno.getMoedas();
+
+		for (int i = 0; i < 12; i++) {
+			Curso curso = new Curso("Curso " + i, 50, false);
+			cursos.add(curso);//addCursos
+		}
+		aluno.setCursos(cursos);//para teste a função setCursos
+		aluno.verCursosETornarPremium();
+
+		assertEquals(moedasBckp + 3, aluno.getMoedas());
+	}
+
+
+}
